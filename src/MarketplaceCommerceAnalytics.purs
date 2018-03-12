bae1a706fade9fdddb7af8a5d6e1cb9225ec6e8d
Philippe Vinchon <pvinchon@gilt.com>
2018-03-12 18:07:54 +0000

    PureScript mapping for aws-sdk-js 2.207.0

--- src/MarketplaceCommerceAnalytics.purs
+++ src/MarketplaceCommerceAnalytics.purs
@@ -20,17 +20,19 @@ import Data.StrMap as StrMap
 import AWS.Request as Request
 import AWS.Request.Types as Types
 
-serviceName = "MarketplaceCommerceAnalytics" :: String
-
 
 -- | Given a data set type and data set publication date, asynchronously publishes the requested data set to the specified S3 bucket and notifies the specified SNS topic once the data is available. Returns a unique request identifier that can be used to correlate requests with notifications from the SNS topic. Data sets will be published in comma-separated values (CSV) format with the file name {data_set_type}_YYYY-MM-DD.csv. If a file with the same name already exists (e.g. if the same data set is requested twice), the original file will be overwritten by the new file. Requires a Role with an attached permissions policy providing Allow permissions for the following actions: s3:PutObject, s3:GetBucketLocation, sns:GetTopicAttributes, sns:Publish, iam:GetRolePolicy.
 generateDataSet :: forall eff. GenerateDataSetRequest -> Aff (exception :: EXCEPTION | eff) GenerateDataSetResult
-generateDataSet = Request.request serviceName "generateDataSet" 
+generateDataSet = Request.request service method  where
+    service = Request.ServiceName "MarketplaceCommerceAnalytics"
+    method = Request.MethodName "generateDataSet"
 
 
 -- | Given a data set type and a from date, asynchronously publishes the requested customer support data to the specified S3 bucket and notifies the specified SNS topic once the data is available. Returns a unique request identifier that can be used to correlate requests with notifications from the SNS topic. Data sets will be published in comma-separated values (CSV) format with the file name {data_set_type}_YYYY-MM-DD'T'HH-mm-ss'Z'.csv. If a file with the same name already exists (e.g. if the same data set is requested twice), the original file will be overwritten by the new file. Requires a Role with an attached permissions policy providing Allow permissions for the following actions: s3:PutObject, s3:GetBucketLocation, sns:GetTopicAttributes, sns:Publish, iam:GetRolePolicy.
 startSupportDataExport :: forall eff. StartSupportDataExportRequest -> Aff (exception :: EXCEPTION | eff) StartSupportDataExportResult
-startSupportDataExport = Request.request serviceName "startSupportDataExport" 
+startSupportDataExport = Request.request service method  where
+    service = Request.ServiceName "MarketplaceCommerceAnalytics"
+    method = Request.MethodName "startSupportDataExport"
 
 
 newtype CustomerDefinedValues = CustomerDefinedValues (StrMap.StrMap OptionalValue)
